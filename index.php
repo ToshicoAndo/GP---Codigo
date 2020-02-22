c1f94735aa2fa001926a3bc78344891556b5ec42
Priscila Ando <priscila.ando@gmail.com>
2020-02-22 08:44:09 -0400

    Codigo original para 8 corrientes

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
       <?php
 		  if($result!==FALSE){
 		     while($row = mysqli_fetch_array($result)) {
-		        printf("<tr><td> &nbsp;%s </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp;%s </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td></tr>",
+		        printf("<tr><td> &nbsp;%s </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td></tr>",
 		           $row["timeStamp"], $row["batV1"], $row["batA1"], $row["batA2"], $row["batA3"], $row["batA4"],$row["batA5"], $row["batA6"], $row["batA7"], $row["batA8"], $row["vel"], $row["flat"], $row["flon"]);
 		     }
 		     mysqli_free_result($result);
